b79abc1d9cbf5c1b222f3e86d87fb0a24a934c60
Marijana Tanovic <tanulica7@gmail.com>
2023-11-20 14:53:03 +0100

    feat: add inMemory storage interface implementation

--- persistence/inmemory.go
+++ persistence/inmemory.go
@@ -7,6 +7,13 @@ import (
 	"github.com/google/uuid"
 )
 
+type Device struct {
+	ID         string // Unique identifier for the device
+	Label      string // Label for the device
+	PublicKey  []byte // Public key associated with the device
+	PrivateKey []byte // Private key associated with the device
+	Algorithm  string // Signature algorithm (ECC or RSA)
+}
 
 type Storage interface {
 	CreateDevice(algorithm, label string) (*Device, error)
@@ -26,4 +33,46 @@ func NewInMemoryStorage() *InMemoryStorage {
 	return &InMemoryStorage{
 		devices: make(map[string]*Device),
 	}
+}
+
+// CreateDevice creates a new signature device and stores it in memory
+func (s *InMemoryStorage) CreateDevice(algorithm, label string) (*Device, error) {
+	publicKeyBytes, privateKeyBytes, err := crypto.GetPairKey(algorithm)
+	if err != nil {
+		return nil, fmt.Errorf("error creating key pair")
+	}
+	// Create a new device
+	newDevice := &Device{
+		ID:         uuid.New().String(),
+		Label:      label,
+		PublicKey:  publicKeyBytes,
+		PrivateKey: privateKeyBytes,
+		Algorithm:  algorithm,
+	}
+
+	// Store the device in the map
+	s.devices[newDevice.ID] = newDevice
+
+	return newDevice, nil
+}
+
+// GetDevice retrieves a signature device by its ID from memory
+func (s *InMemoryStorage) GetDevice(deviceID string) (*Device, error) {
+	device, ok := s.devices[deviceID]
+	if !ok {
+		return nil, fmt.Errorf("device with id: %s doesn't exist", deviceID)
+	}
+
+	return device, nil
+}
+
+// ListDevices retrieves all stored signature devices from memory
+func (s *InMemoryStorage) ListDevices() ([]Device, error) {
+	var deviceList []Device
+
+	for _, device := range s.devices {
+		deviceList = append(deviceList, *device)
+	}
+
+	return deviceList, nil
 }
